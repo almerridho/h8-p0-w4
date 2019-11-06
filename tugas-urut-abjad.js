6b1fc1a7d04135ea5ede1d52451bde4f76b2b8ba
almerridho <56529424+almerridho@users.noreply.github.com>
2019-11-06 11:03:46 +0700

    Add files via upload

---
+++ tugas-urut-abjad.js
@@ -0,0 +1,13 @@
+function urutkanAbjad(str) {
+    var huruf = str.split('')
+    huruf.sort()
+    huruf2 = huruf.join('')
+    return huruf2
+  }
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
